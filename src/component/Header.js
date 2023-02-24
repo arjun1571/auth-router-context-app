ef2ec04399d4141f3a3d2dcb493d59808ee116d8
Arjun Das <arjundasback515@gmail.com>
2023-02-24 12:49:06 +0530

    navigation bar create done

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,6 +6,10 @@ const Header = () => {
         <div>
             <div className="navbar bg-neutral text-neutral-content">
                 <Link to="/home" className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+
+                <Link className='m-5' to="/home">Home</Link>
+                <Link className='m-5' to="/login">Log In</Link>
+                <Link className='m-5' to="/register">Register</Link>
             </div>
         </div>
     );
